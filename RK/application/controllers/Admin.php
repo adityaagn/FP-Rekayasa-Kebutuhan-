97b88eb68790ac375910199f080a0dc4f5afd8df
riansyap <riansyap@gmail.com>
2017-05-15 21:09:14 +0700

    Halaman Progress TA

--- RK/application/controllers/Admin.php
+++ RK/application/controllers/Admin.php
@@ -22,6 +22,15 @@ class Admin extends CI_Controller
 		$this->load->view('Data_kp_admin',$data);
 	}
 
+	function data_ta()
+	{
+		$this->load->model('Model_admin');
+		$NRP = $this->uri->segment(3);
+		$data['mhs']= $this->Model_admin->nama_mhs($NRP)->row_array();
+        $data['dkp'] = $this->Model_admin->kp_mhs($NRP)->result();
+		$this->load->view('Data_ta_admin',$data);
+	}
+
 	function deadline()
 	{
 		$this->load->view('Deadline_admin');
